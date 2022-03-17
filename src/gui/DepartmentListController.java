466ac3f7a40ae382370605d9ee8f2271eb31ff57
Fabio Rocha <fabiolr@gmail.com>
2022-03-16 23:55:45 -0300

    listando na tabela

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -24,12 +30,16 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("Foi clicado no botão");
 	}
 	
-	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -38,13 +48,21 @@ public class DepartmentListController implements Initializable {
 
 
 	private void initializeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("ID"));
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
 		
 		Stage stage = (Stage) Main.getMainscene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 		
 	}
-	
+	public void updateTableView () {
+		if(service == null) {
+			throw new IllegalStateException("Tá nulo filho");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+		
+	}
 
 }
