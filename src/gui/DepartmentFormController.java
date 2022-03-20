4dbde94962e848d411bd91dfbb30fd4b47d8b6ab
Fabio Rocha <fabiolr@gmail.com>
2022-03-20 11:55:14 -0300

    passando obj department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
 
+	private Department entity;
 	
 	@FXML
 	private TextField txtId;
@@ -23,6 +25,11 @@ public class DepartmentFormController implements Initializable {
 	private Button btSave;
 	@FXML
 	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity=entity;
+	}
+
 	
 	@FXML
 	public void onBtSaveAction() {
@@ -43,6 +50,13 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtId, 30);
 		
+	}
+	public void UpdateFormData() {
+		if(entity ==null) {
+			throw new IllegalStateException("Entity está vazia");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 
 }
